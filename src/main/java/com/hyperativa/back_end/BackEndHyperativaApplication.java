751e5143dc4b314b231a6685d765ddabce1263c9
Rafael Carvalho <crcarvalho@outlook.com>
2026-02-10 19:11:59 -0300

    feat(ABC-0001): Add spring artifact

---
+++ src/main/java/com/hyperativa/back_end/BackEndHyperativaApplication.java
@@ -0,0 +1,13 @@
+package com.hyperativa.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndHyperativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndHyperativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperativa/back_end/BackEndHyperativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperativa.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndHyperativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
